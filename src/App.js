2236cfbc96790f05c9ab73941dab794c7bdd116d
Aditya Singh <singhadi280303@gmail.com>
2024-02-26 22:19:34 +0530

    best

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import logo from "./logo.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 import Home from "./Components/Home/Home.js";
-import About from "./Components/About/About.js";
-import Rule from "./Components/Rule/Rule.js";
-import Track from "./Components/Track/Track.js";
-import Contact from "./Components/Contact/Contact.js";
+import PreLoader from "./Components/PreLoader/PreLoader.js";
+
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Simulate loading process
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setIsLoading(false); // Set loading to false after 3000ms
+    }, 3000);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
     <Router>
       <Routes>
-        <Route element={<Home />} path="/" />
-        <Route element={<About />} path="/about" />
-        <Route element={<Rule />} path="/rule" />
-        <Route element={<Track />} path="/track" />
-        <Route element={<Contact />} path="/contact" />
+        {isLoading ? (
+          <Route path="/" element={<PreLoader />} />
+        ) : (
+          <Route path="/" element={<Home />} />
+        )}
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,38 +6,40 @@ import Rule from "../Rule/Rule.js";
 import Main from "../Main/Main.js";
 import Contact from "../Contact/Contact.js";
 import Footer from "../Footer/Footer.js";
-import WAVES from "vanta/dist/vanta.waves.min";
-
+// import WAVES from "vanta/dist/vanta.waves.min";
+import PreLoader from "../PreLoader/PreLoader.js";
 function Home() {
-  const [vantaEffect, setVantaEffect] = useState(null);
-  const myRef = useRef(null);
+  // const [vantaEffect, setVantaEffect] = useState(null);
+  // const myRef = useRef(null);
 
-  useEffect(() => {
-    setVantaEffect(
-      WAVES({
-        el: myRef.current,
-        mouseControls: true,
-        touchControls: true,
-        gyroControls: false,
-        minHeight: 200,
-        minWidth: 200,
-        scale: 1.0,
-        color: 0x191f26,
-        shininess: 20.0,
-        waveSpeed: 0.5,
-        waveHeight: 20.0,
-        zoom: 0.65,
-      })
-    );
+  // useEffect(() => {
+  //   setVantaEffect(
+  //     WAVES({
+  //       el: myRef.current,
+  //       mouseControls: true,
+  //       touchControls: true,
+  //       gyroControls: false,
+  //       minHeight: 200,
+  //       minWidth: 200,
+  //       scale: 1.0,
+  //       color: 0x191f26,
+  //       shininess: 20.0,
+  //       waveSpeed: 0.5,
+  //       waveHeight: 20.0,
+  //       zoom: 0.65,
+  //     })
+  //   );
 
     // Cleanup function
-    return () => {
-      if (vantaEffect) vantaEffect.destroy();
-    };
-  }, []);
+  //   return () => {
+  //     if (vantaEffect) vantaEffect.destroy();
+  //   };
+  // }, []);
 
   return (
-    <div className="container-fluid p-0" ref={myRef}>
+    <div className="container-fluid p-0" style={{backgroundColor:'black'}}>
+      
+
       <Navbar />
       <Main />
       <About />
